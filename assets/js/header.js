0d236f9114b2c8d8622537f1f00c75cc65debfd6
davidevitrano <davide.vitra@gmail.com>
2025-05-09 17:17:56 +0200

    Colors, Font setting and Menu starting setup

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,1 +1,16 @@
 // menu
+
+function navSlide() {
+    const mobilemenu = document.querySelector(".mobile-menu"); //burger
+    const nav = document.querySelector(".nav-links"); //blocco tendina
+    const navLinks = document.querySelectorAll(".nav-links li"); //voci menu
+  
+    //a questo evento associa la funzione
+    mobilemenu.addEventListener("click", ()=> {
+      //Toggle nav se c'è la classe toglila se non c'è mettila
+      nav.classList.toggle("nav-active");
+    });
+  
+  }
+  
+  navSlide();
