05ddc9a3943b28b36746969f71d8d4106f7c4e43
davidevitrano <davide.vitra@gmail.com>
2025-05-12 16:27:46 +0200

    add ActivePage underline

--- assets/js/header.js
+++ assets/js/header.js
@@ -14,3 +14,19 @@ function navSlide() {
     });
   
   }
+
+
+function ActivePage() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const currentPath = window.location.pathname;
+    const links = document.querySelectorAll(".nav-links a");
+
+    links.forEach(link => {
+      if (link.getAttribute("href") === currentPath) {
+        link.classList.add("active-link");
+      }
+    });
+  });
+}
+
+ActivePage()
