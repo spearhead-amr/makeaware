f33fe0edebd7db0b26e5c9ac9cfda49035b2274f
marclura <29801332+marclura@users.noreply.github.com>
2025-07-24 12:42:40 +0200

    add filters and style

--- assets/js/stories-collection/stories-collection.js
+++ assets/js/stories-collection/stories-collection.js
@@ -6,6 +6,7 @@ function loadData() {
     fetch('assets/json/stories-collection.json')
         .then(Response => Response.json())
         .then(data => appendData(data))
+        .then(filterButtonsAddEventListener())
         .catch(error => console.error('Error: ' + error));
 }
 
@@ -21,22 +22,49 @@ function appendData(data) {
         output += `
             <li>
                 <h3>#${i}</h3>
-                I decided to ${story["I decided to"]} - ${story["who"]} 
-                because ${story["because"]}. I was feeling ${story["I was feeling"]} 
-                every time ${story["every time"]}. I've been told by ${story["I've been told by"]} 
-                that I was suffering from ${story["that I was suffering from"]}. It happened ${story["It happened"]} 
-                for ${story["for"]}. The doctor ${story["The doctor"]} to me that ${story["to me that"]}. I started ${story["I started"]}.
-                Ultimately, the cure ${story["Ultimately, the cure"]}. I started feeling ${story["I started feeling"]}. I noticed ${story["I noticed"]}.
-                So, I tried ${story["So, I tried"]}. Now ${story["Now"]}. I am ${story["I am"]}, ${story["Years old"]} years old, 
-                based in ${story["Based in"]}.
+                <span data-filter="diagnosis">I decided to <span class="filter-diagnosis">${story["I decided to"]} ${story["who"]}</span></span> <span data-filter="symptoms">because <span class="filter-symptoms">${story["because"]}</span>.</span>
+                <span data-filter="symptoms">I was feeling <span class="filter-symptoms">${story["I was feeling"]}</span> every time <span class="filter-symptoms">${story["every time"]}</span>.</span>
+                <span data-filter="diagnosis">I've been told by <span class="filter-diagnosis">${story["I've been told by"]}</span> that I was suffering from <span class="filter-diagnosis">${story["that I was suffering from"]}</span>.</span>
+                <span data-filter="diagnosis">It happened <span class="filter-diagnosis">${story["It happened"]} for ${story["for"]}</span>.</span>
+                <span data-filter="treatment">The doctor <span class="filter-treatment">${story["The doctor"]}</span> to me that <span class="filter-treatment">${story["to me that"]}</span>.</span>
+                <span data-filter="treatment">I started <span class="filter-treatment">${story["I started"]}</span>.</span>
+                <span data-filter="treatment">Ultimately, the cure <span class="filter-treatment">${story["Ultimately, the cure"]}</span>.</span>
+                <span data-filter="follow-up">I started feeling <span class="filter-follow-up">${story["I started feeling"]}</span>.</span>
+                <span data-filter="follow-up">I noticed <span class="filter-follow-up">${story["I noticed"]}</span>.</span>
+                <span data-filter="follow-up">So, I tried <span class="filter-follow-up">${story["So, I tried"]}</span>.</span>
+                <span data-filter="follow-up">Now <span class="filter-follow-up">${story["Now"]}</span>.</span>
+                <span data-filter="demographics">I am <span class="filter-demographics">${story["I am"]}, ${story["Years old"]} years old, based in ${story["Based in"]}</span>.</span>
             </li>
         `;
     }
 
-    //console.log(output);
-
     STORIESLIST.innerHTML = output;
 }
 
+const toggleFilterButton = (button) => {
+    const currentFilterElement = document.getElementById("stories-list");
+    const buttonsList = document.getElementById("stories-filters-container").children;
+
+    for(const b of buttonsList) {
+        b.classList.remove('active');
+    }
+    
+    if(currentFilterElement.getAttribute('data-current-filter') != button.dataset.filters) {
+        currentFilterElement.setAttribute('data-current-filter', button.dataset.filters);
+        button.classList.add('active');
+    }
+    else {
+        currentFilterElement.setAttribute('data-current-filter', "");
+        button.classList.remove('active');
+    }
+}
+
+function filterButtonsAddEventListener() {
+    const buttonsList = document.getElementById("stories-filters-container").children;
+
+    for(let i=0; i<buttonsList.length; i++) {
+        buttonsList[i].addEventListener('click', toggleFilterButton.bind(this, buttonsList[i]));
+    }
+}
 
 loadData();
